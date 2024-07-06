5d1856ef165564bce5ba82bbbb4717c627565327
pooya toofan <pooyatoofan@aut.ac.ir>
2024-07-06 23:12:22 +0330

    Fix returning password and bio

--- services/backend/trello_user/serializers.py
+++ services/backend/trello_user/serializers.py
@@ -5,4 +5,4 @@ from .models import TrelloUser
 class TrelloUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrelloUser
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'banner', 'avatar']  # Customize as needed
+        fields = ['first_name', 'last_name', 'username', 'email', 'banner', 'avatar', 'bio']  # Customize as needed
