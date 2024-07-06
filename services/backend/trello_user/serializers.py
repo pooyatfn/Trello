087a6359f8ee0c0640eccbc4e855fe17043a9179
pooya toofan <pooyatoofan@aut.ac.ir>
2024-07-06 23:37:47 +0330

    Fix returning user id

--- services/backend/trello_user/serializers.py
+++ services/backend/trello_user/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
+
 from .models import TrelloUser
 
 
 class TrelloUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrelloUser
-        fields = ['first_name', 'last_name', 'username', 'email', 'banner', 'avatar', 'bio']  # Customize as needed
+        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'banner', 'avatar',
+                  'bio']  # Customize as needed
