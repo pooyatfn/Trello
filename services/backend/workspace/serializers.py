e681086dce56f236c871853a3abb453b2044c4fa
pooya toofan <pooyatoofan@aut.ac.ir>
2024-07-07 17:11:54 +0330

    Fix add user when creating workspace

--- services/backend/workspace/serializers.py
+++ services/backend/workspace/serializers.py
@@ -4,25 +4,60 @@ from trello_user.models import TrelloUser
 from .models import Workspace, WorkspaceMembership
 
 
+class TrelloUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrelloUser
+        fields = ['id', 'username']
+        validators = []
+
+
 class WorkspaceMembershipSerializer(serializers.ModelSerializer):
-    is_admin = serializers.BooleanField()
+    username = serializers.CharField()
 
     class Meta:
         model = WorkspaceMembership
-        fields = ['user', 'is_admin']
+        fields = ['username', 'is_admin']
+        validators = []
+
+    def validate(self, attrs):
+        return attrs
 
 
 class WorkspaceSerializer(serializers.ModelSerializer):
-    users = WorkspaceMembershipSerializer(source='workspacemembership_set', many=True, read_only=True)
+    users = WorkspaceMembershipSerializer(many=True)
 
     class Meta:
         model = Workspace
         fields = ['id', 'name', 'description', 'created_at', 'updated_at', 'users']
+        validators = []
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
-        for user_dict in rep['users']:
-            user_id = user_dict['user']
-            user = TrelloUser.objects.get(id=user_id)
-            user_dict['username'] = user.username
+        user_dicts = rep.get('users', [])
+        for user_dict in user_dicts:
+            username = user_dict['username']
+            user = TrelloUser.objects.get(username=username)
+            membership = WorkspaceMembership.objects.get(workspace=instance, user=user.id)
+            user_dict['is_admin'] = membership.is_admin
         return rep
+
+    def create(self, validated_data):
+        users_data = validated_data['users']
+        workspace_details = {
+            'name': validated_data['name'],
+            'description': validated_data['description']
+        }
+        workspace = Workspace.objects.create(**workspace_details)
+
+        for user_data in users_data:
+            username = user_data['username']
+            is_admin = user_data['is_admin']
+
+            try:
+                user = TrelloUser.objects.get(username=username)
+            except TrelloUser.DoesNotExist:
+                raise serializers.ValidationError(f"A user with username '{username}' does not exist.")
+
+            WorkspaceMembership.objects.create(workspace=workspace, user=user, is_admin=is_admin)
+
+        return workspace
