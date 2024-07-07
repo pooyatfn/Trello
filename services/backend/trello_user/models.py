a0eebf84102465cc536574431c41c79bafd8b05c
pooya toofan <pooyatoofan@aut.ac.ir>
2024-07-07 12:55:37 +0330

    Fix banner avatar URLField

--- services/backend/trello_user/models.py
+++ services/backend/trello_user/models.py
@@ -18,5 +18,5 @@ class TrelloUser(AbstractUser):
     updated_at = models.DateTimeField(default=django.utils.timezone.now, verbose_name='date updated')
     email = models.EmailField(blank=True, max_length=254, verbose_name='email address', unique=True)
     bio = models.CharField(blank=True, max_length=254, verbose_name='biography')
-    banner = models.URLField(null=True, blank=True)
-    avatar = models.URLField(null=True, blank=True)
+    banner = models.CharField(blank=True, max_length=254, verbose_name='banner')
+    avatar = models.CharField(blank=True, max_length=254, verbose_name='avatar')
